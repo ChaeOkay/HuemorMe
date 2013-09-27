830deb30f168709aeeda189e319722ff0ced9293
cokeefe <chaemobile@gmail.com>
2013-09-27 14:51:18 -0700

    mod user

--- spec/features/login_features_spec.rb
+++ spec/features/login_features_spec.rb
@@ -1,17 +1,14 @@
 require 'spec_helper'
 
 feature 'Login' do
-
   context 'valid user' do
-    let(:user) { build(:user) }
-
-    it "should redirect to user show" do
+    let(:user) { create(:user) }
+    it "should redirect to user show", :js => true do
       visit root_path
-      fill_in "user_email", with: user.email
-      fill_in "user_password", with: user.password
-      click_button "sign up"
-      page.current_path.should eq user_path(user)
+      fill_in "session_email", with: user.email
+      fill_in "session_password", with: user.password
+      click_button "login"
+      page.current_path.should eq users_path
     end
   end
-
 end
