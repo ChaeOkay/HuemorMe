b20f2334a01501fe7bfdea9aa75df0cd5b1bc4c9
RainMonster <the.rain.monster@gmail.com>
2013-09-27 16:34:40 -0700

    Bridges controller rspec for #new, #create

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -5,18 +5,21 @@ class BridgesController < ApplicationController
   end
 
   def create
-    # user = User.find(1) #stub
+    user = User.find(params[:user_id]) #stub
     ip = get_local_ip
-    bridge = Bridge.new(user_id: current_user.id, ip: ip )
+    bridge = Bridge.new(user_id: user.id, ip: ip ) #stub
     
     bridge_response = register_user(ip)
     if bridge_response['error']
-      flash[:message] = "Parse the error" #stub
-      redirect_to new_user_bridge_path(current_user) #stub
+      redirect_to new_user_bridge_path(user), notice: "Parse the error" #stub
     else
       bridge.save
-      redirect_to new_bridge_lamp_path(bridge)
+      redirect_to bridge_path(bridge)
     end
+  end
+
+  def show
+    @bridge = Bridge.find(params[:id])
   end
 
 end
