606c3a83c129ebcac6ec257eaa6693f0f53f42fb
Steven Swigart <sbswigart@gmail.com>
2013-09-27 17:08:53 -0700

    add logged_in stub

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -2,4 +2,8 @@ module AuthenticationHelper
   def current_user(user)
     ApplicationController.any_instance.stub(:current_user){ user }
   end
+
+  def logged_in?
+  	ApplicationController.any_instance.stub(:logged_in?){ true }
+  end
 end
