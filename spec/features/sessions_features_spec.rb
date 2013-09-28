6a8970f8762bee5a6c102fc5884ec997da919655
Steven Swigart <sbswigart@gmail.com>
2013-09-27 17:10:20 -0700

    rename from login and add logout

---
+++ spec/features/sessions_features_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature 'Sessions' do
+  context 'valid user login' do
+    let(:user) { create(:user) }
+    it "should redirect to user show", :js => true do
+      visit root_path
+      fill_in "session_email", with: user.email
+      fill_in "session_password", with: user.password
+      click_button "login"
+      page.current_path.should eq user_path(user)
+    end
+  end
+
+  context 'signed in user would be able to click a logout button and be redirected to index' do
+    let(:user) { create(:user) }
+
+    it 'should redirect to index page' do
+      logged_in?
+      visit user_path(user)
+      click_button('logout')
+      page.current_path.should eq root_path
+    end
+  end
+
+end
