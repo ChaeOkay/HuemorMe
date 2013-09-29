84fad6dfa6b3691abe75d63e7d8c0e2923aaabb8
cokeefe <chaemobile@gmail.com>
2013-09-28 21:16:57 -0700

    updated action routes with refactored resources

--- spec/controllers/bridges_controller_spec.rb
+++ spec/controllers/bridges_controller_spec.rb
@@ -2,30 +2,31 @@ require 'spec_helper'
 
 describe BridgesController do
   context "GET #new" do
+    let(:user) { create(:user) }
     it "assigns new @bridge" do
-      get :new
+      get :new, user_id: user
       expect(assigns(:bridge)).to_not be_nil
     end
   end
 
   context "POST #create" do
-    let!(:user) { create(:user) }
-    
+    let(:user) { create(:user) }
+
     before do
       controller.stub(:get_local_ip).and_return("000.000.0.000")
       controller.stub(:current_user).and_return(user)
     end
 
-    it "invalid info redirects to new_user_bridge" do 
+    it "invalid info redirects to new_user_bridge" do
       #No error handling in place
       controller.stub(:register_user).and_return({'error' => "stubbed error message"})
-      post :create
+      post :create, user_id: user
       expect { response }.to redirect_to new_user_bridge_path(user)
       end
 
     it "valid info saves new bridge" do
       controller.stub(:register_user).and_return({})
-      expect { post :create }.to change{ Bridge.all.count }.by(1)
+      expect { post :create, user_id: user }.to change{ Bridge.all.count }.by(1)
     end
   end
 end
