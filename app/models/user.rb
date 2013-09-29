23276528565741478f254969285d1421d73f8c93
RainMonster <the.rain.monster@gmail.com>
2013-09-29 15:30:32 -0700

    username length validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation, :username
   validates_uniqueness_of :email
+  validates :username, length: { 
+    minimum: 10,
+    maximum: 40,
+    too_short: "must be at least 10 characters",
+    too_long: "must be at most 40 characters"
+  }
 
   has_many :bridges
   has_many :lamps, through: :bridges
