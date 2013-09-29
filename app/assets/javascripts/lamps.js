2bc5d452d49a466f2a7bc695d4fad99f9491a3a9
cokeefe <chaemobile@gmail.com>
2013-09-29 16:52:42 -0700

    jqueryui

---
+++ app/assets/javascripts/lamps.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slider-input').slider({
+    min: 0,
+    max: 100,
+    step: 1,
+    slide: function(event, ui){
+      //change value of f.hidden_field brightnes value
+    }
+  })
+})
