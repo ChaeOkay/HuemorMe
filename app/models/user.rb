c7e1c4acec1df404cc581fa2785dce78130cbfda
RainMonster <the.rain.monster@gmail.com>
2013-09-29 15:51:34 -0700

    email regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation, :username
-  validates_uniqueness_of :email
   validates :username, length: { 
     minimum: 10,
     maximum: 40,
     too_short: "must be at least 10 characters",
     too_long: "must be at most 40 characters"
   }
+  validates :email, uniqueness: true,
+    format: {
+      with: /[\w\.\+]+@[\w\.]+\.\w+/,
+        message: "must be valid email address" }  
 
   has_many :bridges
   has_many :lamps, through: :bridges
