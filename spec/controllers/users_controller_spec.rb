d71967cf7264e6d7a67991f2fd5d76d467ed7b29
RainMonster <the.rain.monster@gmail.com>
2013-09-28 21:18:27 -0700

    user show action controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,6 +4,13 @@ describe UsersController do
 
   describe "GET show" do
     let(:user) { build(:user) }
+
+    before do
+      controller.stub(:current_user).and_return(user)
+      controller.stub(:bridge).and_return( Bridge.new(user: user))
+      controller.stub(:lamps).and_return(Lamp.new)
+    end
+
     it "successfully finds show route" do
       get :show, :id => 1
       expect(response).to be_success
--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 feature User do
+
   context "signing up" do
     it "should display a success message" do
       visit root_path
@@ -15,15 +16,51 @@ feature User do
     end
   end
 
-  context "turn on light" do
+  context "user dashboard features" do
     let(:user) { build(:user) }
+    let(:bridge) { FactoryGirl.build(:bridge, user: user) }
+    let(:lamp) { FactoryGirl.build(:lamp, name: "lamp1") }
+    before { @lamps = [lamp] }
+    subject { @lamps }
 
-    it "should change off button to on" do
-      user.stub(:lamps) { [ {"lamp1" => {"status" => { "on" => false }}} ] }
-
+    it "user should see all of their lamps" do
+      assigns(:lamps, [lamp])
+      current_user(user)
       visit user_path(user)
-      click_button "Switch #{lamp1}"
-      expect( page.find(:css, '#lamp1').text ).to eq "off"
+      #binding.pry
+   #    expect(page.body).to include("lamp1")
+   #  end
+   # assign(:widgets, [
+   #    stub_model(Widget, :name => "slicer"),
+   #    stub_model(Widget, :name => "dicer")
+   #  ])
+
+      render
+
+      rendered.should contain("lamp1")
+      end
+
+
+    it "clicking should change off button to on" do
+      current_user(user)
+      visit user_path(user)
+
+      lamp.stub(:say_on_off) { "on" }
+      #lamp.stub_chain(:say_on_off, :on?).and_return("off")
+      lamp.stub(:on?) { false }
+
+
+      #binding.pry
+      click_button "Switch #{lamp.say_on_off}"
+      # lamp.say_on_off.on?.should eq("off")
+      #lamp.any_instance.stub(:on?).and_return( false )
+      
+      #click_button "Switch off"
+      #expect(lamp.say_on_off).to eq("off")
+      expect( page.find(:css, '#{lamp1}').text ).to eq "off"
     end
   end
 end
+      #subject.stub_chain(:one, :two, :three).and_return(:four)
+      #subject.one.two.three.should eq(:four)
+ 
