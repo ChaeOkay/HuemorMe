70bff3910acba7000a90748820c9ce25a8be3157
Steven Swigart <sbswigart@gmail.com>
2013-09-28 18:51:00 -0700

    add device_id to create action

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -6,7 +6,8 @@ class BridgesController < ApplicationController
 
   def create
     ip = get_local_ip
-    bridge = Bridge.new(user_id: current_user.id, ip: ip )
+    device_id = get_device_id
+    bridge = Bridge.new(user_id: current_user.id, ip: ip, device_id: device_id )
     
     bridge_response = register_user(ip)
     if bridge_response['error'] #No error handling in place
