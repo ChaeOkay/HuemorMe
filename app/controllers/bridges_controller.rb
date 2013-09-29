e6d5da728bcb1ec8855bc71dd3a61927b42f58ff
Steven Swigart <sbswigart@gmail.com>
2013-09-29 15:55:11 -0700

    add update action

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -20,4 +20,21 @@ class BridgesController < ApplicationController
   def show
     @bridge = Bridge.find(params[:id])
   end
+
+  def edit
+    @bridge = current_user.bridges.first
+  end
+
+  def update
+    ip = get_local_ip
+    bridge = Bridge.find(params[:id])
+    bridge.update_attributes(ip: ip)
+    bridge_response = register_user(ip)
+    if bridge_response['error']
+      redirect_to edit_user_bridge_path(current_user, bridge), notice: bridge_response['error']
+    else
+      bridge.save
+      redirect_to user_path(current_user)
+    end
+  end
 end
