e6222bed51d15ac14e66980e417a512b2b1031b5
cokeefe <chaemobile@gmail.com>
2013-09-30 11:41:34 -0700

    add group id in lamp creation

--- app/controllers/lamps_controller.rb
+++ app/controllers/lamps_controller.rb
@@ -3,7 +3,8 @@ class LampsController < ApplicationController
   def create
     bridge = Bridge.find(params[:bridge_id])
     get_lights(bridge).each do |lamp|
-      Lamp.create(bridge: bridge, hue_number: lamp)
+      Lamp.create(bridge: bridge, hue_number: lamp,
+        group_id: current_user.groups.find_by_name("All").id)
     end
     redirect_to user_path(current_user)
   end
