26104c2edaf512bb2a6927072618084bcf1b76a2
Steven Swigart <sbswigart@gmail.com>
2013-09-30 11:41:39 -0700

    add ajax to slider and button

--- app/assets/javascripts/lamps.js
+++ app/assets/javascripts/lamps.js
@@ -1,17 +1,50 @@
 var adjustBrightness = function(){
   $('.slider-input').slider({
+  	range: 'max',
+  	min: 1,
+  	max: 255,
+  	value: 0,
     slide: function(event, ui){
       $(this).next().attr('data-brightness', ui.value)
-      $this = $(this).next()
+       $this = $(this).next()
       $.ajax({
-      	type: "POST",
-      	url: '/bridges/' + $this.data('bridge_id') + '/lamps/' + $this.data('lamp_id'),
-      	data: // input params
+      	type: "PATCH",
+      	url: '/bridges/' + $this.data('bridge-id') + '/lamps/' + $this.data('lamp-id'),
+      	data: 'lamp[command]=set_brightness&lamp[data][brightness]=' + ui.value
       })
+    },
+    stop: function(event, ui){
+    	$this = $(this).next()
+      // $.ajax({
+      // 	type: "PATCH",
+      // 	url: '/bridges/' + $this.data('bridge-id') + '/lamps/' + $this.data('lamp-id'),
+      // 	data: 'lamp[command]=set_brightness&lamp[data]=' + ui.value
+      // })
     }
   })
 }
 
+var setBrightnessValue = function(){
+	$('.slider-input').each(function(){
+		number = parseInt($(this).next().attr('data-brightness'))
+		$(this).slider('value', number)
+	})
+}
+
+var toggle_on_off = function(){
+	$(".on_off").on('ajax:success', function(){
+		$button = $(this).children().next().next()
+		if ($button.val() === "Toggle on?"){
+			$button.val("Toggle off?")
+		}
+		else {
+			$button.val("Toggle on?")
+		}
+	})
+}
+
 $(document).ready(function(){
   adjustBrightness()
+  setBrightnessValue()
+  toggle_on_off()
 })
