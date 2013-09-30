bb0f3eb02fa4d876512e24fd204b45ada129c38c
Steven Swigart <sbswigart@gmail.com>
2013-09-30 09:38:00 -0700

    add basic ajax to slider

--- app/assets/javascripts/lamps.js
+++ app/assets/javascripts/lamps.js
@@ -2,6 +2,12 @@ var adjustBrightness = function(){
   $('.slider-input').slider({
     slide: function(event, ui){
       $(this).next().attr('data-brightness', ui.value)
+      $this = $(this).next()
+      $.ajax({
+      	type: "POST",
+      	url: '/bridges/' + $this.data('bridge_id') + '/lamps/' + $this.data('lamp_id'),
+      	data: // input params
+      })
     }
   })
 }
