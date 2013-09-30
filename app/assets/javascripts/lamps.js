9070bf3e5e94bc47889727b68526ed7e137eb5ac
Steven Swigart <sbswigart@gmail.com>
2013-09-30 15:12:14 -0700

    refactor js and jq

--- app/assets/javascripts/lamps.js
+++ app/assets/javascripts/lamps.js
@@ -1,38 +1,45 @@
-var adjustBrightness = function(){
-  $('.slider-input').slider({
-  	range: 'max',
-  	min: 1,
-  	max: 255,
-  	value: 0,
-    slide: function(event, ui){
-      $(this).next().attr('data-brightness', ui.value)
-       $this = $(this).next()
-      $.ajax({
-      	type: "PATCH",
-      	url: '/bridges/' + $this.data('bridge-id') + '/lamps/' + $this.data('lamp-id'),
-      	data: 'lamp[command]=set_brightness&lamp[data][brightness]=' + ui.value
-      })
-    },
-    stop: function(event, ui){
-    	$this = $(this).next()
-      // $.ajax({
-      // 	type: "PATCH",
-      // 	url: '/bridges/' + $this.data('bridge-id') + '/lamps/' + $this.data('lamp-id'),
-      // 	data: 'lamp[command]=set_brightness&lamp[data]=' + ui.value
-      // })
-    }
-  })
-}
+var Lamp = {
+	init: function() {
+		this.adjustBrightness();
+		this.setBrightnessValue();
+		this.toggle_on_off();
+	},
 
-var setBrightnessValue = function(){
-	$('.slider-input').each(function(){
+	adjustBrightness: function(){
+	  $('.slider-input').slider({
+	  	range: 'max',
+	  	min: 1,
+	  	max: 255,
+	  	value: 0,
+	    slide: function(event, ui){
+	      $(this).next().attr('data-brightness', ui.value)
+	       $this = $(this).next()
+	      $.ajax({
+	      	type: "PATCH",
+	      	url: '/bridges/' + $this.data('bridge-id') + '/lamps/' + $this.data('lamp-id'),
+	      	data: 'lamp[command]=set_brightness&lamp[data][brightness]=' + ui.value
+	      })
+	    },
+	    stop: function(event, ui){
+	    	$this = $(this).next()
+	      // $.ajax({
+	      // 	type: "PATCH",
+	      // 	url: '/bridges/' + $this.data('bridge-id') + '/lamps/' + $this.data('lamp-id'),
+	      // 	data: 'lamp[command]=set_brightness&lamp[data]=' + ui.value
+	      // })
+	    }
+	  })
+	},
+
+	setBrightnessValue: function(){
+		$('.slider-input').each(function(){
 		number = parseInt($(this).next().attr('data-brightness'))
 		$(this).slider('value', number)
 	})
-}
+	},
 
-var toggle_on_off = function(){
-	$(".on_off").on('ajax:success', function(){
+	toggle_on_off: function(){
+		$(".on_off").on('ajax:success', function(){
 		$button = $(this).children().next().next()
 		if ($button.val() === "Toggle on?"){
 			$button.val("Toggle off?")
@@ -41,10 +48,9 @@ var toggle_on_off = function(){
 			$button.val("Toggle on?")
 		}
 	})
+	}
 }
 
 $(document).ready(function(){
-  adjustBrightness()
-  setBrightnessValue()
-  toggle_on_off()
+	Lamp.init();
 })
