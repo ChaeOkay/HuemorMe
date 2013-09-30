d2c4a28e5492a20a9104174834c82543876d0ba9
cokeefe <chaemobile@gmail.com>
2013-09-30 13:59:55 -0700

    lamp methods - on/off colorloop bri

--- spec/models/lamp_spec.rb
+++ spec/models/lamp_spec.rb
@@ -5,21 +5,54 @@ describe Lamp do
   it { should belong_to :bridge }
   it { should validate_presence_of :hue_number }
 
+  let(:lamp) { build(:lamp) }
   context "new" do
-    let(:lamp) { build(:lamp) }
     it "should have an on status of false" do
       expect(lamp.on).to eq false
     end
+  end
 
-    it "should be able to turn on" do
-      pending
+  context "#send_command" do
+    let(:response) {
+      {
+        'on' => true,
+        'effect' => 'colorloop',
+        'bri' => '77'
+      }
+    }
 
-      #need to figure out how to
-      #stub the http request
-      #error occuring in making a connection
-
-      # lamp.turn_on_off
-      # expect(lamp.on).to eq true
+    context "#say_on_off?" do
+      it "returns off" do
+        stub_state
+        expect(lamp.say_on_off).to eq "off"
+      end
+      it "returns on" do
+        stub_state({'on' => false})
+        expect(lamp.say_on_off).to eq "on"
+      end
     end
+
+    context "#say_colorloop" do
+      it "returns off" do
+        stub_state
+        expect(lamp.say_colorloop).to eq 'off'
+      end
+      it "returns on" do
+        stub_state({'effect' => 'none'})
+        expect(lamp.say_colorloop).to eq 'on'
+      end
+    end
+
+    context "#say_brightness" do
+      it "returns 77%" do
+        stub_state
+        expect(lamp.say_brightness).to eq '77%'
+      end
+    end
+  end
+
+  def stub_state(options = {})
+    response.merge! options
+    lamp.stub(:state) { response }
   end
 end
