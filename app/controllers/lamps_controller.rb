26a87a742d91245d315ebebb31dc20816e17ff28
Jimmy <jimwong1023@gmail.com>
2013-09-30 17:31:27 -0700

    update controller to fix testing bug

--- app/controllers/lamps_controller.rb
+++ app/controllers/lamps_controller.rb
@@ -3,7 +3,7 @@ class LampsController < ApplicationController
   def create
     bridge = Bridge.find(params[:bridge_id])
     get_lights(bridge).each do |lamp|
-      Lamp.new(bridge: bridge, hue_number: lamp,
+      lamp = Lamp.new(bridge: bridge, hue_number: lamp,
         group_id: current_user.groups.find_by_name("All").id)
       unless lamp.save
         flash[:notice] = lamp.errors.messages[:hue_number]
