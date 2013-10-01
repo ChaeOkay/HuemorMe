45a137bc190e70de3e35caf51268d4c5d025d78d
cokeefe <chaemobile@gmail.com>
2013-09-30 19:54:00 -0700

    update seed with valid password

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@
 user = User.create({ first_name: "Frederick",
                       last_name: "Twistleton",
                       email: "UncleFred@test.com",
-                      password: "test123",
-                      password_confirmation: "test123",
+                      password: "test1234",
+                      password_confirmation: "test1234",
                       username: "unclefredshouse" })
 
 user.groups.create(name: "All")
