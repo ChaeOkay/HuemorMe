9d91df2439f319c3fe3761b879199884d8bdc593
Jimmy <jimwong1023@gmail.com>
2013-10-03 15:44:37 -0700

    refactor controller lamp and updated tests

--- app/controllers/lamps_controller.rb
+++ app/controllers/lamps_controller.rb
@@ -1,15 +1,15 @@
 class LampsController < ApplicationController
 
   def create
-    lamp_array = params[:lamp_ids].split(",")
     flash[:lamp] = []
-    lamp_array.each do |id|
-      lamp = Lamp.new(bridge_id: params[:bridge_id], light_identifier: id)
-      unless lamp.save
-        unless lamp.errors.full_messages == ["Light identifier Cannot register the same light."]
-          flash[:lamp] << lamp.errors.full_messages
-        end
+    if params[:lamp_ids] != ""
+      lamp_array = params[:lamp_ids].split(",")
+      lamp_array.each do |id|
+        lamp = Lamp.new(bridge_id: params[:bridge_id], light_identifier: id)
+        lamp.save
       end
+    else
+      flash[:lamp] << "No lights connected to bridge."
     end
     redirect_to setup_path
   end
