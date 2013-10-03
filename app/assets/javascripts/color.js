1a0f1b0cdb1570f7981234ec4edd4f142549c180
RainMonster <the.rain.monster@gmail.com>
2013-10-03 16:22:38 -0700

    put conditional on event listener

--- app/assets/javascripts/color.js
+++ app/assets/javascripts/color.js
@@ -1,30 +1,36 @@
-function callback(color) {
-	// console.log("callback: " + $.farbtastic('#colorpicker').hsl)
-
+var Color = {
+	init: function() {
+		$("#colorpicker").mouseup(function(){
 			var hsl = getColor()
 			var hue = Math.floor(hsl[0]*65535);
 			var saturation = Math.floor(hsl[1]*255);
 			var brightness 	= Math.floor(hsl[2]*255);
-}
+			console.log("On mouseup: " + hsl)
+			$self = $("#color_setter")
+			      $.ajax({
+			      	type: "GET",
+			      	url: '/bridges/' + $self.data('bridge-id') + '/lamps/' + $self.data('lamp-id') + '/settings',
+			      	data: 'lamp[command]=set_color_and_sat&lamp[data][color]=' + hue + '&lamp[data][saturation]=' + saturation + '&lamp[data][brightness]=' + brightness
+			      })
+		});
+		$.farbtastic('#colorpicker', this.callback)
+	},
 
-function getColor() {
-	return $.farbtastic('#colorpicker').hsl;
-}
+	callback: function(color) {
+	// console.log("callback: " + $.farbtastic('#colorpicker').hsl)
+		var hsl = Color.getColor()
+		var hue = Math.floor(hsl[0]*65535);
+		var saturation = Math.floor(hsl[1]*255);
+		var brightness 	= Math.floor(hsl[2]*255);
+	},
 
-$("#colorpicker").mouseup(function(){
-	var hsl = getColor()
-	var hue = Math.floor(hsl[0]*65535);
-	var saturation = Math.floor(hsl[1]*255);
-	var brightness 	= Math.floor(hsl[2]*255);
-	console.log("On mouseup: " + hsl)
-	$self = $("#color_setter")
-	      $.ajax({
-	      	type: "GET",
-	      	url: '/bridges/' + $self.data('bridge-id') + '/lamps/' + $self.data('lamp-id') + '/settings',
-	      	data: 'lamp[command]=set_color_and_sat&lamp[data][color]=' + hue + '&lamp[data][saturation]=' + saturation + '&lamp[data][brightness]=' + brightness
-	      })
-})
+	getColor: function() {
+		return $.farbtastic('#colorpicker').hsl;
+	}
+};
 
 $(document).ready(function() {
-	$.farbtastic('#colorpicker', callback)
+	if ($('#colorpicker').length > 0) {
+  	Color.init();
+  }
 });
