99f3749cdc922f27d60fcbdb67658b9dd1162ec5
Jimmy <jimwong1023@gmail.com>
2013-10-03 12:13:20 -0700

    cleaned up lib helper method files

--- lib/register_lamps.rb
+++
@@ -1,8 +0,0 @@
-# module RegisterLamps
-
-#   def get_lights(bridge)
-#     uri = URI.parse("http://#{bridge.ip}/api/#{current_user.username}")
-#     MultiJson.load(Net::HTTP.get(uri))['lights'].keys
-#   end
-
-# end
