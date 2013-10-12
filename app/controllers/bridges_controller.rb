e79f82fdcf4dc5f0f7681e03907ba4ea708f80cd
cokeefe <chaemobile@gmail.com>
2013-10-12 06:36:22 -0400

    refactor flash error

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -10,30 +10,13 @@ class BridgesController < ApplicationController
     if bridge.save
       redirect_to bridge_path(bridge)
     else
-      redirect_to new_user_bridge_path(current_user), notice: "An error occured while saving your bridge, please try again."
+      redirect_to new_user_bridge_path(current_user), flash.now[:error] => "An error occured while saving your bridge, please try again."
     end
   end
 
   def show
     @bridge = Bridge.find(params[:id])
   end
-
-  # def edit
-  #   @bridge = current_user.bridges.first
-  # end
-
-  # def update
-  #   ip = get_local_ip
-  #   bridge = Bridge.find(params[:id])
-  #   bridge.update_attributes(ip: ip)
-  #   bridge_response = register_user(ip)
-  #   if bridge_response['error']
-  #     redirect_to edit_user_bridge_path(current_user, bridge), notice: bridge_response['error']
-  #   else
-  #     bridge.save
-  #     redirect_to user_path(current_user)
-  #   end
-  # end
 
   private
 
